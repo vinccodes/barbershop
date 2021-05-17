7fb9b3ba44fa47189225f296a785218c0cc185fe
vinccodes <vinccodes@gmail.com>
2021-05-16 23:13:44 -0400

    Added Home route, views folder, EJS partials, public for static files.

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen('3000', (req, res) =>{
-    console.log('')
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res)=> {
+    res.render('home');
+})
+
+app.listen(3000, (req, res) =>{
+    console.log('Server running on port 3000');
 })
