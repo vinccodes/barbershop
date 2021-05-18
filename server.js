0d3dc7a79799410cfae6f10c8d6a7e9e3dcb4f13
vinccodes <vinccodes@gmail.com>
2021-05-17 23:41:13 -0400

    Added mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const Customer = require('./models/customer')
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
+
+// Setup MongoDB
+mongoose.connect('mongodb://localhost:27017/barbershop', { useNewUrlParser: true})
+.then(() => { 
+    console.log("MONGO CONNECTION OPEN")
+})
+.catch(()=> {
+    console.log("OH NO MONGO CONNECTION ERROR!!")
+    console.log(err);
+})
 
 
 app.get('/', (req, res)=> {
